15c004b220c3c280c0919b6d0d7ef03fcbb602ab
eidan76 <eidan076@gmail.com>
2018-04-06 14:29:38 +0300

    game function + comments

--- sp_nim.c
+++ sp_nim.c
@@ -4,16 +4,21 @@
  *  Created on: Apr 6, 2018
  *      Author: Eidan
  */
+#include "main_aux.h"
 
+/*calculates the computer's next move using nim sum*/
 int* computer_next_move(int *heaps) {
 	int nim_sum = nim_sum(heaps);
 	int i=0;
-	int min_val = -1;
-	int min_index = 0;
+	int min_val = -1; /*stores the current found minimum nim sum*/
+	int min_index = 0; /*index of the heap with the current found minimum nim sum*/
 	int result_move[2];
 	if (nim_sum != 0){
 		for (;i<32;i++) {
-			if (min_val<0 || (min_val > heaps[i] && heaps[i] > heaps[i]^nim_sum)) {
+			/* check if hti > hti XOR st. if so,
+			 * ht1 is the minimum if min_val has not yet been initialized or ht1>min_val
+			 */
+			if (heaps[i] > (heaps[i]^nim_sum) && (min_val<0 || min_val > heaps[i])) {
 				min_val = heaps[i]^nim_sum;
 				min_index = i;
 			}
@@ -22,7 +27,10 @@ int* computer_next_move(int *heaps) {
 	}
 	else {
 		for (;i<32;i++) {
-			if (min_val<0 || (min_val > heaps[i] && heaps[i] > 0)) {
+			/* check if hti > 0. if so,
+			 * ht1 is the minimum if min_val has not yet been initialized or ht1>min_val
+			 */
+			if (heaps[i] > 0 && (min_val<0 || min_val > heaps[i])) {
 				min_val = heaps[i];
 				min_index = i;
 			}
@@ -32,7 +40,7 @@ int* computer_next_move(int *heaps) {
 	result_move[0] = min_index;
 	return result_move;
 }
-
+/*calculates the nim sum of the entire heap*/
 int nim_sum(int *heaps) {
 	int sum = 0;
 	for (int i=0;i<32;i++) {
@@ -40,7 +48,7 @@ int nim_sum(int *heaps) {
 	}
 	return sum;
 }
-
+/*checks if all heaps are empty*/
 int is_game_over(int *heaps) {
 	for (int i=0;i<32;i++) {
 		if (heaps[i] > 0)
@@ -49,3 +57,24 @@ int is_game_over(int *heaps) {
 	return 1;
 }
 
+int game(int *heaps) {
+	int whos_turn = 1; /*1=user 0=computer*/
+	int turn = 1; /*turn counter*/
+	int turn_request[2]; /*each turn is summed up as {index of heap to remove from, no. of objects to remove}*/
+	while (!is_game_over(heaps)) {
+		whos_turn = !whos_turn;
+		print_board(turn,heaps);
+
+		if (whos_turn == 0)
+			turn_request = computer_next_move(heaps);
+		else
+			turn_request = user_next_move(heaps);
+
+		print_move(whos_turn, turn_request[1], turn_request[0]);
+		heaps[turn_request[0]] -= turn_request[1]; /* remove X objects from heap Y*/
+
+		turn++;
+	}
+	print_board(turn,heaps);
+	print_winner(whos_turn);
+}
